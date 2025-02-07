5d7b2196b022e0cc4283a8a778802c39342645f7
valeriy korovin <1k9o9r5@gmail.com>
2025-02-07 21:46:37 +0200

    '4.3_Filling out Contact Item'

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import ContactsList from "./Components/ContactsList.jsx";
 
--- src/Components/ContactItem.jsx
+++ src/Components/ContactItem.jsx
@@ -1,8 +1,22 @@
-function ContactItem() {
-    return (
-        <div>
+import {useDispatch} from "react-redux";
+import {Button, Card, CardContent, Stack, Typography} from "@mui/material";
 
-        </div>
+function ContactItem({contact}) {
+    const dispatch = useDispatch()
+
+    return (
+        <Card sx={{mb: 2, p: 1}}>
+            <CardContent>
+                <Typography></Typography>
+                <Typography></Typography>
+                <Typography></Typography>
+                <Stack>
+                    <Button>
+
+                    </Button>
+                </Stack>
+            </CardContent>
+        </Card>
     );
 }
 
