0c2a4507a0eea3668639e95028ef182def032fd3
valeriy korovin <1k9o9r5@gmail.com>
2025-02-08 00:24:18 +0200

    '5.1_Create Contact Form'

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import ContactsList from "./Components/ContactsList.jsx";
+import {Container} from "@mui/material";
 
 function App() {
 
   return (
     <>
-      <ContactsList/>
+        <Container sx={{mt:1}}>
+            <ContactsList/>
+        </Container>
+
     </>
   )
 }
