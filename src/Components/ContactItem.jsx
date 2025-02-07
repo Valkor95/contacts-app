331c106d9c888f7c96a635b410556248c99c56d3
valeriy korovin <1k9o9r5@gmail.com>
2025-02-08 00:07:08 +0200

    '4.4_Filling out Contact Item_end'

--- src/Components/ContactItem.jsx
+++ src/Components/ContactItem.jsx
@@ -1,18 +1,24 @@
 import {useDispatch} from "react-redux";
 import {Button, Card, CardContent, Stack, Typography} from "@mui/material";
+import {deleteContact} from "../../store/slice/contactSlice.js";
 
+// eslint-disable-next-line react/prop-types
 function ContactItem({contact}) {
     const dispatch = useDispatch()
 
     return (
         <Card sx={{mb: 2, p: 1}}>
             <CardContent>
-                <Typography></Typography>
-                <Typography></Typography>
-                <Typography></Typography>
-                <Stack>
-                    <Button>
-
+                <Typography variant="h6">{contact.name}</Typography>
+                <Typography variant="body1">Phone: {contact.phone}</Typography>
+                <Typography variant="body2" color="text.secondary">Category: {contact.category}</Typography>
+                <Stack direction="row" spacing={1} sx={{mt:1}}>
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        onClick={() => dispatch(deleteContact(contact.id))}
+                    >
+                        Delete
                     </Button>
                 </Stack>
             </CardContent>
