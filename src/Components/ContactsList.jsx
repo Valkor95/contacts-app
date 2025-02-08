0969a32f03e6bf712c33a4f8bcdc6ac22771c7cb
valeriy korovin <1k9o9r5@gmail.com>
2025-02-08 19:30:31 +0200

    '6.3_Update Contacts List + add search filter'

--- src/Components/ContactsList.jsx
+++ src/Components/ContactsList.jsx
@@ -1,13 +1,23 @@
 import {useSelector} from "react-redux";
 import {List, Typography} from "@mui/material";
 import ContactItem from "./ContactItem.jsx";
+import {useState} from "react";
 
 function ContactsList() {
     const contacts = useSelector((state) => state.contacts.contacts)
+    const [searchTerm, setSearchTerm] = useState("");
+    const [category, setCategory] = useState("All")
+
+    const filteredContacts = contacts.filter((contact) => {
+        const matchesSearch = contact.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                                contact.phone.includes(searchTerm);
+        const matchesCategory = category === "All" || contact.category === category;
+        return matchesSearch && matchesCategory
+    })
 
     return (
         <>
-            <Typography variant="h5" gutterBottom>
+            <Typography variant="h2" gutterBottom>
                 Contact list
             </Typography>
             {contacts.length === 0 ? (
