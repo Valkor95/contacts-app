0ab3a0da233c98c398d7197705f460a868a5fb72
valeriy korovin <1k9o9r5@gmail.com>
2025-02-09 15:05:47 +0200

    '8.5_Filling out Edit contact modal'

--- src/Components/EditContactModal.jsx
+++ src/Components/EditContactModal.jsx
@@ -1,18 +1,28 @@
 import {Dialog, DialogContent, DialogTitle, MenuItem, Stack, TextField} from "@mui/material";
 import {useDispatch, useSelector} from "react-redux";
 import {closeEditModal} from "../../store/slice/contactSlice.js";
+import {useFormState} from "react-dom";
+
+
 
 function EditContactModal() {
     const dispatch = useDispatch()
     const {openEditModal, contactToEdit} = useSelector((state) => state.contacts)
 
+    const initialState = {...contactToEdit}
+
+    const formAction = (prevState, formData) => {
+
+    }
+
+    const [state, action] = useFormState(formAction, initialState)
     return (
         <Dialog open={openEditModal} onClose={() => dispatch(closeEditModal())}>
             <DialogTitle>
                 EDIT THIS CONTACT
             </DialogTitle>
             <DialogContent>
-                <form>
+                <form action={action}>
                     <Stack spacing={2} sx={{mt: 1}}>
                         <TextField label="Name" name="name" defaultValue={contactToEdit.name} required/>
                         <TextField label="Phone" name="phone" defaultValue={contactToEdit.phone} required/>
